415f5b34ad3a8eda990cb3da079d0845195e02a8
Laura <Laura@mail.com>
2023-10-28 12:44:20 -0300

    se agrega vista login, registro y dos menu

---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Usuario());
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
