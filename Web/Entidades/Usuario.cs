80b09a605d4625b777c9bf632370ebc4b112357d
Laura <Laura@mail.com>
2023-10-28 18:31:53 -0300

    clase usuario views

---
+++ Web/Entidades/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Usuario
+{
+    [Required]
+    public string Email {  get; set; }
+    [Required]
+    public string Username { get; set; } 
+    [Required]
+    public string Password { get; set; } 
+
+}
