8b78540531cee4e0af2fb8de6487f90dcb4363be
Saleck3 <aledagonale@gmail.com>
2023-11-04 14:58:37 -0300

    Corregidas referencias

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,174 +1,171 @@
-﻿using Api.Entidades;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using Microsoft.AspNetCore.Authorization;
 using System.Data;
-using Api.Logica;
-using Api.EF;
-using System.Collections;
 using System.Text;
+using Api.Logica;
+using Api.Entidades;
+using Api.EF;
 
-namespace Api.Controllers
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class AuthController : ControllerBase
+
+    private readonly IConfiguration _configuration;
+    private IUsuarioServicio _usuariosServicio;
+    private IIngresoServicio _ingresoServicio;
+    //private readonly TimeZoneInfo _timeZone;
+
+
+    public AuthController(IConfiguration configuration, IUsuarioServicio uServicio, IIngresoServicio iServicio)//, IUserService userService)
     {
-        
-        private readonly IConfiguration _configuration;
-        private IUsuarioServicio _usuariosServicio;
-        private IIngresoServicio _ingresoServicio;
-        //private readonly TimeZoneInfo _timeZone;
+        _configuration = configuration;
+        //_userService = userService;
+        //_timeZone = timeZone;
+        _usuariosServicio = uServicio;
+        _ingresoServicio = iServicio;
+    }
 
-
-        public AuthController(IConfiguration configuration, IUsuarioServicio uServicio, IIngresoServicio iServicio)//, IUserService userService)
+    [HttpPost("Registro")]
+    public async Task<ActionResult<Usuario>> Registro(UsuarioDto request)
+    {
+        try
         {
-            _configuration = configuration;
-            //_userService = userService;
-            //_timeZone = timeZone;
-            _usuariosServicio = uServicio;
-            _ingresoServicio = iServicio;
+            Usuario user = new Usuario();
+            CreatePasswordHash(request.Password, ref user);
+            user.Username = request.Username;
+            _usuariosServicio.Crear(user);
+            return Ok(user);
         }
-        
-        [HttpPost("Registro")]
-        public async Task<ActionResult<Usuario>> Registro(UsuarioDto request)
+        catch (Exception)
         {
-            try
-            {
-                Usuario user = new Usuario();
-                CreatePasswordHash(request.Password, ref user);
-                user.Username = request.Username;
-                _usuariosServicio.Crear(user);
-                return Ok(user);
-            }
-            catch (Exception ) {
-                return BadRequest("User Exists.");
-            }
+            return BadRequest("User Exists.");
         }
+    }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<string>> Login(UsuarioDto request)
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UsuarioDto request)
+    {
+        Usuario user = (Usuario)_usuariosServicio.Filtrar(request.Username).First();
+        if (user == null)
         {
-            Usuario user = (Usuario)_usuariosServicio.Filtrar(request.Username).First();
-            if (user == null)
-            {
-                return BadRequest("User not found.");
-            }
-
-            if (!VerifyPasswordHash(request.Password, ref user))
-            {
-                return BadRequest("Wrong password.");
-            }
-
-            RegistrarIngreso(ref user);
-
-            string token = CreateToken(ref user);
-
-            var refreshToken = GenerateRefreshToken();
-            SetRefreshToken(refreshToken, ref user);
-
-            return Ok(token);
+            return BadRequest("User not found.");
         }
 
-        private RefreshToken GenerateRefreshToken()
+        if (!VerifyPasswordHash(request.Password, ref user))
         {
-            var refreshToken = new RefreshToken
-            {
-                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
-                Expires = DateTime.Now.AddMinutes(2),
-                Created = DateTime.Now
-            };
-
-            return refreshToken;
+            return BadRequest("Wrong password.");
         }
 
-        private void SetRefreshToken(RefreshToken nuevoRefreshToken, ref Usuario user)
-        {
-            var cookieOptions = new CookieOptions
-            {
-                HttpOnly = true,
-                Expires = nuevoRefreshToken.Expires,
-            };
+        RegistrarIngreso(ref user);
 
-            Response.Cookies.Append("refreshToken", nuevoRefreshToken.Token, cookieOptions);
-            user.Refreshtoken = nuevoRefreshToken.Token;
-            user.Tokencreated = nuevoRefreshToken.Created;
-            user.Tokenexpires = nuevoRefreshToken.Expires;
+        string token = CreateToken(ref user);
+
+        var refreshToken = GenerateRefreshToken();
+        SetRefreshToken(refreshToken, ref user);
+
+        return Ok(token);
+    }
+
+    private RefreshToken GenerateRefreshToken()
+    {
+        var refreshToken = new RefreshToken
+        {
+            Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+            Expires = DateTime.Now.AddMinutes(2),
+            Created = DateTime.Now
+        };
+
+        return refreshToken;
+    }
+
+    private void SetRefreshToken(RefreshToken nuevoRefreshToken, ref Usuario user)
+    {
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Expires = nuevoRefreshToken.Expires,
+        };
+
+        Response.Cookies.Append("refreshToken", nuevoRefreshToken.Token, cookieOptions);
+        user.Refreshtoken = nuevoRefreshToken.Token;
+        user.Tokencreated = nuevoRefreshToken.Created;
+        user.Tokenexpires = nuevoRefreshToken.Expires;
+    }
+
+
+    [HttpPost("refresh-token")]
+    public async Task<ActionResult<string>> RefreshToken(Usuario user)
+    {
+        var refreshToken = Request.Cookies["refreshToken"];
+        if (!user.Refreshtoken.Equals(refreshToken))
+        {
+            return Unauthorized("Refresh Token inválido");
+        }
+        else if (user.Tokenexpires < DateTime.Now)
+        {
+            return Unauthorized("El token expiró");
         }
 
- 
-        [HttpPost("refresh-token")]
-        public async Task<ActionResult<string>> RefreshToken(Usuario user)
-        {
-            var refreshToken = Request.Cookies["refreshToken"];
-            if (!user.Refreshtoken.Equals(refreshToken))
-            {
-                return Unauthorized("Refresh Token inválido");
-            }else if(user.Tokenexpires < DateTime.Now)
-            {
-                return Unauthorized("El token expiró");
-            }
+        string token = CreateToken(ref user);
+        var nuevoRefreshToken = GenerateRefreshToken();
+        SetRefreshToken(nuevoRefreshToken, ref user);
+        return Ok(token);
+    }
 
-            string token = CreateToken(ref user);
-            var nuevoRefreshToken = GenerateRefreshToken();
-            SetRefreshToken(nuevoRefreshToken, ref user);
-            return Ok(token);
+    private void CreatePasswordHash(string password, ref Usuario usuario)
+    {
+
+        using (var hmac = new HMACSHA512())
+        {
+            usuario.Passwordsalt = hmac.Key;
+            usuario.Passwordhash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
         }
 
-        private void CreatePasswordHash(string password, ref Usuario usuario)
+        usuario.Password = Encoding.UTF8.GetString(usuario.Passwordhash);
+    }
+
+    private bool VerifyPasswordHash(string password, ref Usuario user)
+    {
+        using (var hmac = new HMACSHA512(user.Passwordsalt))
         {
-            
-            using (var hmac = new HMACSHA512())
-            {
-                usuario.Passwordsalt = hmac.Key;
-                usuario.Passwordhash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-
-            usuario.Password = Encoding.UTF8.GetString(usuario.Passwordhash); 
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(user.Passwordhash);
         }
+    }
 
-        private bool VerifyPasswordHash(string password, ref Usuario user)
+    private void RegistrarIngreso(ref Usuario user)
+    {
+        Ingreso ingreso = new Ingreso();
+        ingreso.UserId = user.Id;
+        ingreso.User = user;
+        _ingresoServicio.Crear(ingreso);
+    }
+
+    private string CreateToken(ref Usuario usuario)
+    {
+        List<Claim> claims = new List<Claim>
         {
-            using (var hmac = new HMACSHA512(user.Passwordsalt))
-            {
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                return computedHash.SequenceEqual(user.Passwordhash);
-            }
-        }
+            new Claim("nombre", usuario.Username),
+            new Claim("roles", "Admin")
+        };
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+            _configuration.GetSection("Token").Value));
 
-        private void RegistrarIngreso(ref Usuario user)
-        {
-            Ingreso ingreso = new Ingreso();
-            ingreso.UserId = user.Id;
-            ingreso.User = user;
-            _ingresoServicio.Crear(ingreso);
-        }
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-        private string CreateToken(ref Usuario usuario)
-        {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim("nombre", usuario.Username),
-                new Claim("roles", "Admin")
-            };
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
-                _configuration.GetSection("Token").Value));
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(2),
+            signingCredentials: creds);
 
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(2),
-                signingCredentials: creds);
-
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return jwt;
-        }
+        return jwt;
     }
 }
--- Web/Entidades/Usuario.cs
+++ Web/Entidades/Usuario.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
+namespace Web;
 public class Usuario
 {
     [Required]
-    public string Email {  get; set; }
+    public string? Email { get; set; }
     [Required]
-    public string Username { get; set; } 
+    public string? Username { get; set; } 
     [Required]
-    public string Password { get; set; } 
-
+    public string? Password { get; set; }
 }
