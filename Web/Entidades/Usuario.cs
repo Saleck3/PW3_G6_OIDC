186ef37df5d94ba86fb2883a853e05c827d98b0a
Laura <Laura@mail.com>
2023-11-12 20:53:46 -0300

    se agrega validaciones en el front

--- Web/Entidades/Usuario.cs
+++ Web/Entidades/Usuario.cs
@@ -4,9 +4,11 @@ namespace Web.Entidades
 {
     public class Usuario
     {
+        [StringLength(25, MinimumLength = 4, ErrorMessage = "La longitud del nombre de usuario debe estar entre 4 y 25 caracteres")]
         [Required(ErrorMessage = "El nombre de usuario es requerido")]
         public string? Username { get; set; }
 
+        [StringLength(250, MinimumLength = 8, ErrorMessage = "La longitud de la clave debe estar entre 8 y 250 caracteres")]
         [Required(ErrorMessage = "La clave es requerida")]
         public string? Password { get; set; }
     }
